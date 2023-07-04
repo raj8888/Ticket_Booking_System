8859e3ce9f98c41af207057bb0f60b42c9c48269
raj8888 <jadhavrj8877@gmail.com>
2023-07-04 16:35:31 +0530

    Added: Added userschema

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema({
+    name:{type:String,required: true,unique: true},
+    email:{type:String,required: true,unique: true},
+    mobile:{type:String,required: true,unique: true},
+    createdDate:String,
+    password:String,
+    role:String,
+})
+
+const userModel=mongoose.model("Users",userSchema)
+
+module.exports={
+    userModel
+}
