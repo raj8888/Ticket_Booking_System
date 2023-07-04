c3f6b833607cf7e072150c233606638ca5f9ffd9
raj8888 <jadhavrj8877@gmail.com>
2023-07-04 23:09:19 +0530

    Added: Create schema for movies and tickets

---
+++ backend/models/movies.model.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose")
+
+const moviesSchema=mongoose.Schema({
+    movieName:String,
+    totalTickets:Number,
+    totalPlatiniumTickets:Number,
+    totalGoldTickets:Number,
+    totalSilverTickets:Number,
+    bookedPlatiniumSeats:{type: [String]},
+    bookedGoldSeats:{type: [String]},
+    bookedSilverSeats:{type: [String]},
+    closingDate:String,
+    createdDate:String
+})
+
+const movieModel=mongoose.model("Movies",moviesSchema)
+
+module.exports={
+    movieModel
+}
